564cc0580653034a4cb2e51d50f0410ed9135fce
leegyurak <dev@devgyurak.com>
2025-12-22 22:02:31 +0900

    FIX: mypy

--- fetcher/src/infrastructure/external/genius_lyrics_repository.py
+++ fetcher/src/infrastructure/external/genius_lyrics_repository.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import logging
 
-import lyricsgenius  # type: ignore[import-untyped]
+import lyricsgenius
 
 from src.domain.entities.song import Song
 from src.domain.repositories.lyrics_repository import LyricsRepository
